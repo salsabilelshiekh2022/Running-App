64041fc8073301184033a40e0035c5ca6485773b
salsabileslam <salsabileslam16@gmail.com>
2023-10-21 23:00:36 +0300

    Router

---
+++ lib/core/router/router.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+Route<dynamic>? onGenerate(RouteSettings settings) => null;
+final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
+class AppRouter {
+  static BuildContext context = navigatorKey.currentContext!;
+
+  static Future<dynamic> navigateTo(Widget page) =>
+      navigatorKey.currentState!.push(_materialPageRoute(page));
+
+  static Future<dynamic> navigateAndReplace(Widget page) =>
+      navigatorKey.currentState!.pushReplacement(_materialPageRoute(page));
+
+  static Future<dynamic> navigateAndPop(Widget page) =>
+      navigatorKey.currentState!.pushAndRemoveUntil(
+        _materialPageRoute(page),
+        (_) => false,
+      );
+
+  static void pop([Object? result]) => navigatorKey.currentState!.pop(result);
+
+  static Route<dynamic> _materialPageRoute(Widget page) =>
+      MaterialPageRoute(builder: (_) => page);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:running_app/core/router/router.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,7 +11,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Running App',
+      debugShowCheckedModeBanner: false,
+      navigatorKey: navigatorKey,
+      onGenerateRoute: onGenerate,
     );
   }
 }
